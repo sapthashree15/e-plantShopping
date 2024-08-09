ee17ac048f8e655c54e2b3f1f89a59662dc438cb
sapthashree15 <kssapthashree@gmail.com>
2024-08-09 05:10:01 -0400

    changes made from theia lab environment

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -3,33 +3,62 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
-const CartItem = ({ onContinueShopping }) => {
+
+const CartItem = ({ onContinueShopping, addedToCart, reactivateAddButton}) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
+  const calculateTotalAmount = () => {     
+    
+    const totalAmount=cart.reduce((total,item)=>total+item.quantity*parseInt(item.cost.replace("$","")),0);   
+    
+    return totalAmount;
+
+
  
   };
-
+  const handleCheckoutShopping = (e) => {
+    alert('Coming Soon!!');
+  };
   const handleContinueShopping = (e) => {
+    onContinueShopping(e)
    
   };
 
 
 
   const handleIncrement = (item) => {
+    const quantity=item.quantity+1;   
+    dispatch(updateQuantity({name:item.name,quantity:quantity}));
+    
+    
+    
   };
 
   const handleDecrement = (item) => {
+    if (item.quantity-1>0){
+        const quantity=item.quantity-1;
+        dispatch(updateQuantity({name:item.name,quantity:quantity}));
+    }else if (item.quantity-1==0){
+        dispatch(removeItem(item.name));
+        reactivateAddButton(item);
+    }
+
    
   };
 
   const handleRemove = (item) => {
+    
+    dispatch(removeItem(item.name));
+    reactivateAddButton(item);
+    
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const itemcost=parseInt(item.cost.replace("$",""))
+    return item.quantity*itemcost;
   };
 
   return (
@@ -57,12 +86,10 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e)=>handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
-
-
