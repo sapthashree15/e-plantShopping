f5e55501ac6aeb47546a432b6de869fe7bb74c8c
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2025-10-10 10:28:51 +0530

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -11,22 +11,7 @@ function ProductList() {
     const [addedToCart, setAddedToCart] = useState({});
     const cartItems = useSelector((state) => state.cart.items);
 
-    const calculateTotalQuantity = () => {
-        return cartItems ? cartItems.reduce((total, item) => total + item.quantity, 0) : 0;
-    };
-    const handleParadiseNurseyClick = () => {
-        e.preventDefault();
-        setShowPlants(false);
-        setShowCart(false);
-    };
-
-    const reactivateAddButton = (product) => {
-        setAddedToCart((prevState) => ({
-            ...prevState,
-            [product.name]: false, // Set the product name as key and value as false to indicate it's removed from cart
-        }));
-    }
-
+  
     const checkDisabled = (name) => {
         if (name in addedToCart) {
             if (addedToCart[name] === true) {
@@ -159,19 +144,7 @@ function ProductList() {
                 <div className="tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <a href="/new-e-plantshopping" style={{ textDecoration: 'none' }} onClick={(e) => handleParadiseNurseyClick(e)}>
-                            <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
-                            </div>
-                        </a>
-                    </div>
-
-                </div>
-                <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg><div className='cart_quantity_count'>{totalQuantity}</div></h1></a></div>
-                </div>
+                       
             </div>
             {!showCart ? (
                 <div className="product-grid">
@@ -181,12 +154,7 @@ function ProductList() {
                             <div className="product-list">
                                 {category.plants.map((plant, plantIndex) => (
                                     <div className="product-card" key={plantIndex}>
-                                        <img className="product-image" src={plant.image} alt={plant.name} />
-                                        <div className="product-title">{plant.name}</div>
-                                        <div className="product-description">{plant.description}</div>
-                                        <div className="product-cost">{plant.cost}</div>
-                                        {/*Similarly like the above plant.name show other details like description and cost*/}
-                                        <button className="product-button" onClick={() => handleAddToCart(plant)} disabled={checkDisabled(plant.name)}>Add to Cart</button>
+                                       
                                     </div>
                                 ))}
                             </div>
