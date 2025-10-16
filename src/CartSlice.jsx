13b59702b462736778d3fcb576d5a810a2b0e0fa
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2025-10-16 10:56:28 +0530

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -20,7 +20,10 @@ export const CartSlice = createSlice({
     },
     updateQuantity: (state, action) => {
       const { name, quantity } = action.payload;
-     
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
